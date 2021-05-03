04862b7ff0249e2f7aa17c370f157075e943ee46
EC2 Default User <ec2-user@ip-172-31-27-223.ec2.internal>
2021-05-03 18:46:31 +0000

    initial commit

--- pycdk/pycdk_stack.py
+++ pycdk/pycdk_stack.py
@@ -4,8 +4,10 @@ from aws_cdk import core as cdk
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
+from aws_cdk import ( 
+    aws_s3 as s3,
+    core
+)
 
 class PycdkStack(cdk.Stack):
 
@@ -13,3 +15,11 @@ class PycdkStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        bucket = s3.Bucket(
+            self,
+            id='s3cdkbucket',
+            bucket_name='cdkbucket42876',
+            versioned=True,
+            encryption=s3.BucketEncryption.KMS_MANAGED
+        )
